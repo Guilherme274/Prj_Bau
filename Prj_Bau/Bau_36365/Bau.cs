d4e99b30b493f52511f9452e158ac787804875e7
Guilherme Paixão dos Santos <111549138+Guilherme274@users.noreply.github.com>
2023-03-07 10:38:59 -0300

    Add files via upload

---
+++ Prj_Bau/Bau_36365/Bau.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bau_36365
+{
+    internal class Bau
+    {
+        #region Criação das Propriedades
+
+        private bool _estadoBau;
+        public bool EstadoBau
+        {
+            get
+            {
+                return _estadoBau;
+            }
+            set
+            {
+                if(BauTrancado == true)
+                {
+                    _estadoBau = false;
+                }
+                else
+                {
+                    _estadoBau = value;
+                }
+                    
+                
+            }
+        }
+        
+
+        private bool _bautrancado;
+
+        public bool BauTrancado
+        {
+            get
+            {
+                return _bautrancado;
+            }
+            set
+            {
+                    
+                    _bautrancado = value;
+                
+            }
+        }
+
+        private int _qtdMoedas;
+        public int QtdMoedas
+        {
+            get
+            {
+                return _qtdMoedas;
+            }
+            set
+            {
+                if (value >= 0 || value <= 200)
+                {
+                    _qtdMoedas = value;
+                }
+               else
+                {
+                    throw new Exception("O baú suporta até apenas 200 moedas");
+                }
+            }
+
+        }
+
+        #endregion
+
+        #region Criação dos Métodos
+        public string Abrir()
+        {
+           
+                EstadoBau = true;
+                return "O Baú foi aberto, aperte ENTER para coletar as moedas";
+           
+        }
+
+        public string TrancadoOuDestrancado()
+        {
+            if(BauTrancado == true)
+            {
+                
+                return "O Baú está Trancado, aperte ENTER para destranca-lo";
+            }
+            else
+            {
+                
+
+                return BauAbertoOuFechado();
+            }
+        }
+        public string BauAbertoOuFechado()
+        {
+            if(BauTrancado == false)
+
+            { 
+                if (EstadoBau == true)
+                {
+    
+
+                   if(QtdMoedas == 0)
+                    {
+                        return "O Baú está aberto mas está vazio";
+                    }
+                   else
+                    {
+                        return "O Baú está aberto, para coletar as moedas aperte ENTER";
+                    }
+                    
+
+                }
+                else
+                {
+
+                    return "O Baú está fechado, aperte ENTER para abri-lo";
+                    
+
+                }
+           
+            }
+            else
+            {
+                return Destrancar();
+            }
+        }
+
+        public string Destrancar()
+        {
+            if(BauTrancado == true)
+            {
+                
+                return "O Baú foi destrancado, aperte ENTER para abri-lo";
+            }
+            else
+            {
+               
+                return "O Baú está destrancado";
+            }
+        }
+
+
+        public string ColetarMoedas()
+        {
+           if(QtdMoedas == 0)
+            {
+                return "O Baú está vazio";
+            }
+           else
+            {
+                return "As moedas foram coletadas, você coletou " + QtdMoedas + " moedas";
+            }
+           
+        }
+        #endregion
+
+    }
+}
---
+++ Prj_Bau/Bau_36365/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bau_36365
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Definição dos Objetos
+
+
+            Bau bau1 = new Bau();
+            
+            
+            bau1.QtdMoedas = 200;
+            bau1.BauTrancado = false;
+            bau1.EstadoBau = false;
+
+            Bau bau2 = new Bau();
+
+
+            bau2.QtdMoedas = 120;
+            bau2.BauTrancado = true;
+            
+
+            Bau bau3 = new Bau();
+
+           
+            bau3.QtdMoedas = 0;
+            bau3.EstadoBau = true;
+            bau3.BauTrancado = false;
+
+            Bau bau4 = new Bau();
+
+
+            bau4.QtdMoedas = 34;
+            bau4.EstadoBau = true;
+            bau4.BauTrancado = false;
+
+
+
+            #endregion
+
+            #region Chamada dos Baús
+
+
+            Console.WriteLine("Bem vindo ao programa ");
+
+            Console.WriteLine("Possibilidade 1: Baú Destrancado e Fechado com moedas");
+
+            Console.Write("\n");
+
+            Console.WriteLine(bau1.TrancadoOuDestrancado());
+
+            Console.Write("\n");
+
+            Console.ReadKey();
+
+            Console.WriteLine(bau1.Abrir());
+
+            Console.Write("\n");
+
+            Console.ReadKey();
+
+            Console.WriteLine(bau1.ColetarMoedas());
+
+
+            Console.Write("\n");
+
+            Console.WriteLine("Aperte ENTER para prosseguir");
+
+            Console.ReadKey();
+
+            Console.Write("\n");
+
+            Console.WriteLine("Possibilidade 2: Baú Trancado com moedas");
+
+            Console.WriteLine();
+
+            Console.WriteLine(bau2.TrancadoOuDestrancado());
+
+            Console.Write("\n");
+
+            Console.ReadKey();
+
+            Console.WriteLine(bau2.Destrancar());
+
+            Console.Write("\n");
+
+            Console.ReadKey();
+
+            Console.WriteLine(bau2.Abrir());
+
+            Console.Write("\n");
+
+            Console.ReadKey();
+
+            Console.WriteLine(bau2.ColetarMoedas());
+
+            Console.Write("\n");
+
+
+            Console.WriteLine("Aperte ENTER para prosseguir");
+
+
+            Console.ReadKey();
+
+
+
+            Console.Write("\n");
+
+            Console.WriteLine("Possibilidade 3: Baú Destrancado e Aberto possuindo 0 moedas");
+
+            Console.Write("\n");
+
+            Console.WriteLine(bau3.TrancadoOuDestrancado());
+
+            Console.Write("\n");
+
+            Console.Write("\n");
+
+
+            Console.WriteLine("Aperte ENTER para prosseguir");
+
+            Console.ReadKey();
+
+            Console.Write("\n");
+
+            Console.WriteLine("Possibilidade 4: Baú Destrancado e Aberto com moedas");
+
+            Console.Write("\n");
+
+            Console.WriteLine(bau4.TrancadoOuDestrancado());
+
+            Console.Write("\n");
+
+            Console.ReadKey();
+
+            Console.WriteLine(bau4.ColetarMoedas());
+
+            Console.Write("\n");
+
+            Console.WriteLine("Obrigado por testar todas as possibilidades do meu baú");
+
+            Console.ReadKey();
+
+            #endregion
+        }
+
+
+
+
+    }
+}
